ab06f75ac8e1a3016d9c1d3a9f8ce6d85c931735
billmr777 <153546020+billmr777@users.noreply.github.com>
2026-01-29 21:24:43 +0100

    Add files via upload

---
+++ Desp/ P-UD6-JAVADOC-PHPDOC/PHPDoc/funciones.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Suma dous valores.
+ *
+ * @param int|float $a O primeiro número
+ * @param int|float $b O segundo número
+ * @return int|float A suma de $a e $b
+ */
+function sumar($a, $b) {
+    return $a + $b;
+}
+?>
